0670477bedd818404ea5132a1b0fc36456609716
SamanthaDison <samnicdis@gmail.com>
2021-12-08 14:20:08 -0700

    JS arrays added

---
+++ app.js
@@ -0,0 +1,46 @@
+// const iceCream = [{ name: 'Cookie Dough', image: 'https://celebratingsweets.com/wp-content/uploads/2014/04/Cookie-Dough-Ice-Cream-1-5.jpg', price: 1 },
+// { name: 'Vanilla', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ultimate-vanilla-ice-cream-1628511695.jpg', price: 1 },
+// { name: 'Strawberry', image: 'https://www.realfoodwithjessica.com/wp-content/uploads/2017/07/paleostrawberryicecream2.jpg', price: 2 }]
+
+const iceCreams = [{
+    name: 'Vanilla',
+    price: 2.00,
+    Image: 'https://www.tasteofhome.com/wp-content/uploads/2018/10/vanilla-ice-cream-scoop.jpg?fit=700,800'
+},
+{
+    name: 'Chocolate',
+    price: 2.00,
+    Image: 'https://i2.wp.com/lifemadesimplebakes.com/wp-content/uploads/2015/07/Brownie-Fudge-Swirl-Ice-Cream.jpg'
+},
+{
+    name: 'Strawberry',
+    price: 2.00,
+    Image: 'https://i0.wp.com/www.alattefood.com/wp-content/uploads/2015/05/Homemade-Strawberry-Ice-Cream-Recipe-12-up-close.jpg?resize=800%2C1360&ssl=1'
+},
+{
+    name: 'Cookie Dough',
+    price: 3.00,
+    Image: 'https://www.chiselandfork.com/wp-content/uploads/2021/06/chocolate-chip-cookie-dough-ice-cream-2.jpg'
+}]
+
+const vessels = [{
+    name: 'Waffle Cone',
+    price: 2.00,
+    image: 'https://m.media-amazon.com/images/I/71VNjBMakfL._SL1500_.jpg'
+},
+{
+    name: 'Waffle Bowl',
+    price: 4.00,
+    image: 'http://images.wbmason.com/350/L_JOY66050.jpg'
+}]
+
+const toppings = [{
+    name: 'Sprinkles',
+    price: 1.00,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg'
+},
+{
+    name: 'Chocolate Chips',
+    price: 2.00,
+    image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360'
+}]
